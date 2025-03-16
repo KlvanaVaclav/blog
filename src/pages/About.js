5c1f014357f8ef0b1e619b0b7a7ad28a194356b7
Václav Klvaňa <klvana.vaclav@seznam.cz>
2025-03-16 12:31:07 +0100

    blog basic page structure

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+    return (
+        <div>
+            <h1>About This Blog</h1>
+            <p>Welcome to my blog! Here you'll find posts about various topics.</p>
+        </div>
+    );
+}
+
+export default About;
---
+++ src/pages/BlogPost.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function BlogPost() {
+    return (
+        <div>
+            <h1>Blog Posts</h1>
+            <p>Here you will find different blog posts.</p>
+        </div>
+    );
+}
+
+export default BlogPost;
